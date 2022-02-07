9c907e421d37575ace022d9bfdf8ae624435fa9e
husein zolkepli <husein.zol05@gmail.com>
2022-02-07 09:15:42 +0800

    added conf

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # postgREST
+
 general RDBMS, If you part of the organization, you can access to postgREST! https://postgrest.malaysiaai.ml
---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt install postgresql -y
+wget https://github.com/PostgREST/postgrest/releases/download/v9.0.0/postgrest-v9.0.0-linux-static-x64.tar.xz
+tar -xf postgrest-v9.0.0-linux-static-x64.tar.xz
+sudo cp postgrest /usr/bin
